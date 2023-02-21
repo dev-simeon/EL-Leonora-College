6210f5ed20cabb952f1ea3e362b1c8a213053a2c
dev-simeon <omomowosymeon45@gmail.com>
2023-02-21 14:01:25 +0100

    changes

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,24 @@
 let body = document.querySelector("body");
 let navbar = document.querySelector("nav");
+let navLinks = document.querySelectorAll(".navlink-lg")
 let backgroundImg = document.querySelector("#hero-page");
 let changeNavbarPosition = backgroundImg.offsetHeight;
 
-body.addEventListener("scroll",() => {
-    if (window.scrollY >= changeNavbarPosition) {
-        navbar.classList.add(".d-none")
+
+function ChangeNavbarAppearance() {
+    if (window.pageYOffset >= changeNavbarPosition) {
+        navbar.classList.add("bg-transparent");
+        navbar.classList.add("shadow");
+        navLinks.forEach(element => {
+            element.style.color = "#666D83";
+        });
+        
     } 
     else {
-        console.log("hey");
+        navbar.classList.remove("bg-transparent");
+        navbar.classList.remove("shadow");
+        navLinks.forEach(element => {
+            element.style.color = "#FFFFFF";
+        });
     }
-});
-
-console.log(changeNavbarPosition)
+};
