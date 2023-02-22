4788f278e14fdbcfb3aa7e8562aafaec3e6f9168
dev-simeon <omomowosymeon45@gmail.com>
2023-02-22 15:21:17 +0100

    push

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,27 @@
-let body = document.querySelector("body");
+let bodyPage = document.querySelector("body");
 let navbar = document.querySelector("nav");
 let navLinks = document.querySelectorAll(".navlink-lg")
 let backgroundImg = document.querySelector("#hero-page");
 let changeNavbarPosition = backgroundImg.offsetHeight;
 
 
-body.addEventListener("scroll",() => {
+bodyPage.onscroll = () => {
     ChangeNavbarAppearance();
-});
+};
 
 function ChangeNavbarAppearance() {
     if (window.pageYOffset >= changeNavbarPosition) {
         navbar.classList.add("bg-white");
         navbar.classList.add("shadow-sm");
+        navbar.classList.add("opacity-75");
         navLinks.forEach(element => {
             element.style.color = "#666D83";
         });
-        
     } 
     else {
         navbar.classList.remove("bg-white");
         navbar.classList.remove("shadow-sm");
+        navbar.classList.remove("opacity-75");
         navLinks.forEach(element => {
             element.style.color = "#FFFFFF";
         });
